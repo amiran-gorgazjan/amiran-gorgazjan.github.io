94755e190b7a8af46026e8cb791d1d2979ec9c6a
amiran-gorgazjan <amiran.gorgazjan@gmail.com>
2023-12-13 09:33:01 +0100

    Test.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/_styles");
+    eleventyConfig.addPassthroughCopy("src/styles");
     eleventyConfig.addPassthroughCopy("src/test.css");
 };
